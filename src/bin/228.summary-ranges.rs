79ce19b1d47bdb1e90b54a8ae57ed590ea804c71
Ancient77 <76914086+Ancient77@users.noreply.github.com>
2023-06-12 08:21:36 +0000

    228 - rust (daily)

--- src/bin/228.summary-ranges.rs
+++ src/bin/228.summary-ranges.rs
@@ -11,9 +11,17 @@ impl Solution {
     pub fn summary_ranges(nums: Vec<i32>) -> Vec<String> {
         let mut res = Vec::new();
         let mut n = 0;
+        if(nums.len() == 2){
+            if(nums[1] - nums[0] == 1)
+            {
+            let a = nums[0].min(nums[1]);
+            let b = nums[0].max(nums[1]);;
+            return vec![format!("{a}->{b}")];
+
+            }
+        }
         while n < nums.len() {
             let a = nums[n];
-            
             let mut b = None;
             while nums.get(n+1).unwrap_or(&-1) - b.unwrap_or(a) == 1 {
                 n = n+1;
