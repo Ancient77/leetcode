31a9f405d4f4db5bf47ba50d290430ebb81ac294
Ancient77 <76914086+Ancient77@users.noreply.github.com>
2023-06-29 12:48:26 +0200

    2703 - typescript

---
+++ src/bin/2703.return-length-of-arguments-passed.ts
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode id=2703 lang=typescript
+ *
+ * [2703] Return Length of Arguments Passed
+ */
+
+// @lc code=start
+function argumentsLength(...args: any[]): number {
+    return args.length
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
+// @lc code=end
+
